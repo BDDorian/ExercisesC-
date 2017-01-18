7a8d34b4eb12df349283c0f5ab0a5dbc9cf6e133
Dorian <boudrique.dorian@gmail.com>
2017-01-18 15:28:55 +0100

    FicherDL

---
+++ ConsoleApplication7/Class1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication7
+{
+    // Ma classe que je veux mettre
+    class Heros
+    {
+        private string nom;
+        private string prenom;
+        private int age;
+        private short level;
+        private string classe;
+        private string race;
+
+        //Constructeurs
+     
+        public Heros()
+        {
+            this.nom = "";
+        }
+
+        // Mutateurs
+
+        public void setnom(string nom)
+        {
+            this.nom = nom;
+        }
+
+
+        // Accesseurs
+
+        public string getnom()
+        {
+            return this.nom;
+        }
+
+        // Nos propres méthodes
+
+        public void affichernom()
+        {
+            Console.WriteLine("Le nom du personnage  est:" + this.nom);
+        }
+
+    }
+}
+    
---
+++ ConsoleApplication7/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Heros Clad = new Heros();
+            Heros Sephiroth = new Heros();
+            Heros Barrett = new Heros();
+            Heros Tifa = new Heros();
+
+            Clad.affichernom();
+            Sephiroth.affichernom();
+            Barrett.affichernom();
+            Tifa.affichernom();
+        }
+    }
+}
